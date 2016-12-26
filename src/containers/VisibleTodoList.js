c269e2d0eb692a856726d66c7994a669d6b724c5
Emre Bayram <endemre@yahoo.com>
2016-12-26 22:54:43 +0200

    -

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,41 +1,29 @@
 /**
  * Created by kazanture on 12/26/16.
  */
-class VisibleTodoList extends Component {
-    componentDidMount() {
-        const {store} =this.context;
-        this.unsubscribe = store.subscribe(() =>
-            this.forceUpdate()
-        );
-    }
 
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render () {
-        const props = this.props;
-        const {store} = this.context;
-        const state = store.getState();
-
-        return (
-            <TodoList
-                todos={
-                    getVisibleTodos(
-                        state.todos,
-                        state.visibilityFilter
-                    )
-                }
-                onTodoClick={id =>
-                store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id
-                })
-                }
-                />
-        );
-    }
-}
-VisibleTodoList.contextTypes = {
-    store: React.PropTypes.object
+const mapStateToProps=(state)=> {
+    return {
+        todos:  getVisibleTodos(
+            state.todos,
+            state.visibilityFilter
+        )
+    };
 };
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      onTodoClick: id => {
+          dispatch({
+              type: 'TOGGLE_TODO',
+              id
+          })
+        }
+  } ;
+};
+
+const { connect } = ReactRedux;
+const VisibleTodoList = connect (
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList);
